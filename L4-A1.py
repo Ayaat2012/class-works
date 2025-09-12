e67d62c550f0f3d6e0bc7f7771b205e43c0369e8
Ayaat2012 <iffatrajshahi@gmail.com>
2025-09-12 19:21:56 +0600

    initial commit

---
+++ L4-A1.py
@@ -0,0 +1,29 @@
+# 1. Import Tkinter Module
+# 2. Create the GUI application main Window
+# 3. Add widgets
+from tkinter import *
+
+window = Tk()
+window.title = ('Tkinter Sample Window')
+window.geometry = ('300 x 300')
+
+#Label
+greeting = Label(text="Hello user", fg='black', bg='white')
+#Button
+button = Button(text="Click me", fg='white', bg='black')
+#Entry
+entry = Entry(fg='yellow', bg='blue', width=100)
+
+greeting.pack()
+button.pack()
+entry.pack()
+
+frame = Frame(master=window, relief=RAISED, borderwidth=5)
+frame.pack()
+label = Label(master=frame, text="Sample Frame")
+label.pack()
+
+textbox = Text(fg='green', bg='white')
+textbox.pack()
+
+window.mainloop()
